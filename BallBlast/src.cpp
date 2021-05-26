e71d16299d6b5d343f1d10c938d128346c60d1f3
F7E9E5 <sth3353@gmail.com>
2021-05-26 21:08:46 +0900

    코드 수정 (충돌 좌표, 간략화) 및 주석 추가

--- BallBlast/src.cpp
+++ BallBlast/src.cpp
@@ -30,6 +30,7 @@ struct cannonInfo {
 	int speed;
 	int power;
 };
+
 struct ballInfo {
 	ObjectID object;
 	int x;
@@ -41,6 +42,7 @@ struct ballInfo {
 	bool item;
 	bool life;
 };
+
 struct bulletInfo {
 	ObjectID object;
 	int x;
@@ -49,15 +51,17 @@ struct bulletInfo {
 	const int speed = 10;
 	bool life;
 };
+
 struct itemInfo {
 	ObjectID object;
 	int x;
 	int y;
-	int type; //0->armor 1->powerup
+	int type; //0->armor 1->X2
 	double speedY;
 	bool used;
 	bool exist;
 };
+
 struct stageButtonInfo {
 	ObjectID object;
 	bool flag;
@@ -84,7 +88,7 @@ ObjectID returnButton = createObject("image//return.png");
 TimerID endTimer = createTimer(3.0f);
 TimerID animeTimer = createTimer(0.01f);
 
-//create stage buttons
+//스테이지 flag에 따라 스테이지 선택 버튼 생성 후 배치
 void stageButton() {
 	char ch[30];
 
@@ -97,7 +101,7 @@ void stageButton() {
 	}
 }
 
-//cannon image change for armor item / type true -> armor on false -> armor off
+//armor아이템 상태에 따라 대포의 이미지 변경
 void cannonChange(bool type) {
 	hideObject(cannon.object);
 	if (type) cannon.object = createObject("image//cannonArmor.png");
@@ -107,38 +111,38 @@ void cannonChange(bool type) {
 	showObject(cannon.object);
 }
 
-//return ball's scale
+//공의 크기를 리턴
 double ballScaleCal(ballInfo* ball) {
 	return 0.06 * ((double)ball->size + 1);
 }
 
-//return distance between (ax, ay) (bx, by)
+//(ax, ay) (bx, by) 두 좌표 간의 거리 계산
 double distanceCal(int ax, int ay, int bx, int by) {
 	return sqrt(pow((double)ax - bx, 2) + pow((double)ay - by, 2));
 }
 
-//return ball's radious
+//공의 반지름 리턴
 int ballRad(ballInfo* ball) {
 	return 500 * ballScaleCal(ball);
 }
 
-//return ball's center X
+//공 중심 x좌표
 int ballCenterX(ballInfo* ball) {
 	return ball->x + ballRad(ball);
 }
 
-//return ball's center Y
+//공 중심 y좌표
 int ballCenterY(ballInfo* ball) {
 	return ball->y + ballRad(ball);
 }
 
-//check at least one ball's size is stageNumber - 1
+//적어도 하나의 공이 stageNumber-1의 사이즈를 가지게 하여 난이도 유지
 bool difficultyCheck() {
 	for (int i = 0; i < BALLNUMBER; i++) if (balls[i].size >= stageNumber - 1) return false;
 	return true;
 }
 
-//randomize ball feature when game starts
+//게임 시작 시 공 생성
 void initialBallSet() {
 	char ch[30];
 
@@ -156,115 +160,96 @@ void initialBallSet() {
 		scaleObject(balls[i].object, ballScaleCal(balls + i));
 		showObject(balls[i].object);
 	}
-
 	currentBall = BALLNUMBER;
 }
 
-//locate balls
+//공 위치시키기
 void locateBall() {
 	for (int i = 0; i < currentBall; i++)
 		if (balls[i].life) locateObject(balls[i].object, game, balls[i].x, balls[i].y);
 }
 
-//locate bullets
+//총알 위치시키기
 void locateBullet() {
 	for (int i = 0; i < BULLETNUMBER; i++) 
 		if (bullets[i].life) locateObject(bullets[i].object, game, bullets[i].x, bullets[i].y);
 }
 
-//locate items
+//아이템 위치시키기
 void locateItem() {
 	for (int i = 0; i < 2; i++) 
 		if (items[i].exist) locateObject(items[i].object, game, items[i].x, items[i].y);
 }
 
-//create new bullet
+//새로운 총알 생성 (덱 구조)
 void fireBullet() {
 	char ch[50];
-
 	int t = currentBulletEnd++ % BULLETNUMBER;
+
 	bullets[t].power = cannon.power;
 	sprintf_s(ch, "image//bullet%d.png", bullets[t].power);
 	bullets[t].object = createObject(ch);
 	bullets[t].x = cannon.x + 35;
 	bullets[t].y = 113;
 	bullets[t].life = true;
-
 	scaleObject(bullets[t].object, 0.05f);
 	locateObject(bullets[t].object, game, bullets[t].x, bullets[t].y);
 	showObject(bullets[t].object);
 }
 
-//check bullet <-> ball
+//총알과 공이 충돌했는지 검사
 void hitBullet() {
 	int randnum;
 	char ch[30];
 
 	for (int i = 0; i < currentBall; i++) {
-		if (balls[i].life) {
+		if (balls[i].life) {									   //공이 살아있을 경우에만
 			for (int j = 0; j < BULLETNUMBER; j++) {
-				if (bullets[j].life &&
-					ballRad(balls + i) >=
+				if (bullets[j].life &&							   //총알이 살아있고
+					ballRad(balls + i) >=						   //공의 중심과 총알과의 거리가 공의 반지름보다 작을 때
 					distanceCal(bullets[j].x + 25, bullets[j].y + 37, ballCenterX(balls + i), ballCenterY(balls + i))) {
 
 					bullets[j].life = false;
 					hideObject(bullets[j].object);
 					
-					//if ball broken
-					if (balls[i].power - bullets[j].power <= 0) {
+					if (balls[i].power - bullets[j].power <= 0) {   //공의 체력이 0 이하인 경우
 						balls[i].life = false;
 						hideObject(balls[i].object);
 
-						//if ball is bigger than ball1 then two ball created
+						//공 두 개로 나누기
 						if (balls[i].size > 0) {
-							balls[currentBall].x = balls[i].x;
-							balls[currentBall].y = balls[i].y;
-							balls[currentBall].size = balls[i].size - 1;
-							balls[currentBall].power = pow(2, balls[currentBall].size);
-							balls[currentBall].speedX = 2 * (rand() % 5 - 2);
-							balls[currentBall].speedY = -0.3;
-							balls[currentBall].item = false;
-							balls[currentBall].life = true;
-							sprintf_s(ch, "image//ball%d.png", balls[currentBall].size + 1);
-							balls[currentBall].object = createObject(ch);
+							for (int k = 0; k < 2; k++) {
+								balls[currentBall].size = balls[i].size - 1;
+								balls[currentBall].x = ballCenterX(balls + i) - ballRad(balls + currentBall);
+								balls[currentBall].y = ballCenterY(balls + i) - ballRad(balls + currentBall );							
+								balls[currentBall].power = pow(2, balls[currentBall].size);
+								balls[currentBall].speedY = -0.3;
+								balls[currentBall].item = false;
+								balls[currentBall].life = true;
+								sprintf_s(ch, "image//ball%d.png", balls[currentBall].size + 1);
+								balls[currentBall].object = createObject(ch);
+								scaleObject(balls[currentBall].object, ballScaleCal(balls + currentBall));
+								showObject(balls[currentBall++].object);
+							}
 
-							scaleObject(balls[currentBall].object, ballScaleCal(balls + currentBall));
-							showObject(balls[currentBall].object);
-
-							currentBall++;
-
-							balls[currentBall].x = balls[i].x;
-							balls[currentBall].y = balls[i].y;
-							balls[currentBall].size = balls[i].size - 1;
-							balls[currentBall].power = pow(2, balls[currentBall].size);
-							do {
-								randnum = rand() % 5;
-							} while (balls[currentBall - 1].speedX == 2 * (randnum - 2));
-							balls[currentBall].speedX = 2 * (randnum % 5 - 2);
-							balls[currentBall].speedY = -0.3;
-							balls[currentBall].item = false;
-							balls[currentBall].life = true;
-							sprintf_s(ch, "image//ball%d.png", balls[currentBall].size + 1);
-							balls[currentBall].object = createObject(ch);
-
-							scaleObject(balls[currentBall].object, ballScaleCal(balls + currentBall));
-							showObject(balls[currentBall].object);
-
-							currentBall++;
+							//나뉘어진 두 개의 공의 수평속도를 일치하지 않도록 함
+							balls[currentBall - 2].speedX = 2 * (rand() % 5 - 2);
+							do { randnum = 2 * (rand() % 5 - 2);
+							} while (balls[currentBall - 2].speedX == randnum);
+							balls[currentBall - 1].speedX = randnum;
 						}
 
-						//if broken ball has item
+						//만약 부서진 공이 아이템을 가졌다면
 						if (balls[i].item) {
 							balls[i].item = false;
 
-							//find item that not exist
-							do {
-								randnum = rand() % 2;
+							//아직 사용되지 않은 아이템을 뽑는다
+							do { randnum = rand() % 2;
 							} while (items[randnum].used);
 							items[randnum].used = true;
 							items[randnum].exist = true;
 
-							//locate item at broken ball's center
+							//아이템의 중심과 공의 중심을 일치시킨다
 							items[randnum].x = ballCenterX(balls + i) - 50;
 							items[randnum].y = ballCenterY(balls + i) - 50;
 							items[randnum].speedY = 0;
@@ -280,7 +265,7 @@ void hitBullet() {
 	}
 }
 
-//if a bullet is too high, kill it
+//총알이 화면 밖으로 벗어났을 시 제거
 void bulletInSky() {
 	for (int i = 0; i < BULLETNUMBER; i++) {
 		if (bullets[i].life) {
@@ -292,18 +277,18 @@ void bulletInSky() {
 	}
 }
 
-//check whether all balls die
+//모든 공이 부서졌을 경우 true를 리턴
 bool clearCheck() {
 	for (int i = 0; i < currentBall; i++) if (balls[i].life) return false;
 	return true;
 }
 
-//check cannon <-> ball
+//대포와 공의 충돌을 검사
 bool failCheck() {
-	if (armorFlag) return false;
-	for (int i = 0; i < currentBall; i++) {
-		if (balls[i].life &&
-			(ballRad(balls + i) >=
+	if (armorFlag) return false;	//armor아이템 활성화 상태이면 검사 안함
+	for (int i = 0; i < currentBall; i++) {  
+		if (balls[i].life &&		//공이 살아있고
+			(ballRad(balls + i) >=	//대포의 세 점과 공의 중심과의 거리 중 하나라도 공의 반지름보다 작으면 true
 			 distanceCal(cannon.x + 60, cannon.y + 102, ballCenterX(balls + i), ballCenterY(balls + i)) ||
 			 ballRad(balls + i) >=
 			 distanceCal(cannon.x + 17, cannon.y + 50, ballCenterX(balls + i), ballCenterY(balls + i)) ||
@@ -314,19 +299,18 @@ bool failCheck() {
 	return false;
 }
 
-//select which ball have item
+//아이템을 가질 공을 랜덤하게 뽑는다
 void selectItemBall() {
 	int number;
 
 	for (int i = 0; i < 2; i++) {
-		do {
-			number = rand() % BALLNUMBER;
+		do { number = rand() % BALLNUMBER;
 		} while (balls[number].item);
 		balls[number].item = true;
 	}
 }
 
-//item initialize
+//아이템 초기화
 void itemMaker() {
 	items[0].object = createObject("image//armor.png");
 	items[1].object = createObject("image//powerup.png");
@@ -338,20 +322,20 @@ void itemMaker() {
 	}	
 }
 
-//check cannon <-> item
+//대포와 아이템의 충돌 체크
 void itemCheck() {
 	for (int i = 0; i < 2; i++) {
-		if (items[i].exist) {
-			if (items[i].y + 35 <= 103 && cannon.x + 108 >= items[i].x + 20 && cannon.x + 12 <= items[i].x + 80) {
+		if (items[i].exist) {						//아이템이 화면에 존재할 시
+			if (items[i].y <= 68 && cannon.x + 88 >= items[i].x && cannon.x - 68 <= items[i].x) {
 				hideObject(items[i].object);
 				items[i].exist = false;
 
-				if (items[i].type == 0) {
+				if (items[i].type == 0) {			//아이템 armor
 					armorFlag = true;
 					armorTimer = 1000;
 					cannonChange(true);
 				}
-				else if (items[i].type == 1) {
+				else if (items[i].type == 1) {      //아이템 X2
 					cannon.power = 2;
 					powerFlag = true;
 					powerTimer = 1000;
@@ -361,7 +345,7 @@ void itemCheck() {
 	}
 }
 
-//start game
+//게임 시작
 void gameStart() {
 	gameflag = true;
 	enterScene(game);
@@ -374,26 +358,25 @@ void gameStart() {
 	cannonChange(false);
 
 	itemMaker();
-	do {
-		initialBallSet();
+	do { initialBallSet();
 	} while (difficultyCheck());	
 	selectItemBall();
 }
 
-//if game cleared
+//게임 클리어 시
 void clearGame() {
 	showObject(clearImage);
 	startTimer(endTimer);
 	stages[stageNumber].flag = true;
 }
 
-//if game failed
+//게임 실패 시
 void failGame() {
 	showObject(failImage);
 	startTimer(endTimer);
 }
 
-//when game clear or fail
+//게임이 종료했을 시
 void gameEnd(bool param) {
 	stopTimer(animeTimer);
 	gameflag = false;
@@ -438,58 +421,56 @@ void keyboardCallBack(KeyCode code, KeyState state) {
 }
 
 void timerCallBack(TimerID timer) {
-	if (timer == animeTimer) {
+	if (timer == animeTimer) {	//0.01초
 		if (failCheck()) gameEnd(false);
 		else if (clearCheck()) gameEnd(true);
 
-		//move cannon
-		if (keyflag == true) {
-			if (cannon.x + cannon.speed > 1160) cannon.x = 1160;
-			else if (cannon.x + cannon.speed < 0) cannon.x = 0;
+		//대포의 x좌표를 조정
+		if (keyflag == true) {   //키보드 눌린 상태일 때
+			if (cannon.x + cannon.speed >= 1160) cannon.x = 1160;
+			else if (cannon.x + cannon.speed <= 0) cannon.x = 0;
 			else cannon.x += cannon.speed;
-
 			locateObject(cannon.object, game, cannon.x, cannon.y);
 		}
 
-		//move ball
+		//공의 x와 y좌표 조정
 		for (int i = 0; i < currentBall; i++) {
-			if (balls[i].life) {		
-				if (balls[i].x <= 0 || balls[i].x + 2 * ballRad(balls + i) >= 1280)			//ball hit wall
-					balls[i].speedX *= -1;
+			if (balls[i].life) {														//공이 살아있으면
+				if (balls[i].x <= 0 || balls[i].x + 2 * ballRad(balls + i) >= 1280)		//공이 벽에 부딫히면
+					balls[i].speedX *= -1;												//동일한 속력으로 튕겨나옴
 				
 				balls[i].speedY += 0.05;
-				if (balls[i].y - balls[i].speedY <= 0)										//ball hit ground
-					balls[i].speedY = -1 * ((double)(rand() % 5) + 6);		
-				else if (balls[i].y - balls[i].speedY >= 720) balls[i].speedY = -0.3;
+				if (balls[i].y - balls[i].speedY <= 0)									//바닥에 공이 부딫히면
+					balls[i].speedY = -1 * ((double)(rand() % 5) + 6);					//랜덤으로 튀어오르는 속도 조정
+				else if (balls[i].y - balls[i].speedY >= 720) balls[i].speedY = -0.3;	//너무 위로 가지 않도록 조정
 			
 				balls[i].x += balls[i].speedX;
 				balls[i].y -= balls[i].speedY;
 			}
 		}
 
-		//move bullet
+		//총알 y좌표 조정
 		for (int i = 0; i < BULLETNUMBER; i++) if (bullets[i].life) bullets[i].y += bullets[i].speed;
 
-		//move item
+		//아이템 y좌표 조정
 		for (int i = 0; i < 2; i++) {
-			if (items[i].exist) {
-				if (items[i].y - items[i].speedY <= 0) items[i].y = 0;		//ball on ground
-				else {
+			if (items[i].exist) {											//아이템이 화면에 존재할 시
+				if (items[i].y - items[i].speedY <= 0) items[i].y = 0;		//아이템이 바닥에 있으면 가만히
+				else {														//아니면 떨어뜨림
 					items[i].speedY += 0.05;
 					items[i].y -= items[i].speedY;
 				}
 			}	
 		}
 
-		//bullet frequency
-		if (bulletTemp++ >= bulletFrequency) {
-			fireBullet();
-			bulletTemp = 0;
-		}
+		//총알을 일정 주기마다 발사하도록 함
+		if (bulletTemp++ >= bulletFrequency) { fireBullet(); bulletTemp = 0; }
 
-		//armor timer
+		//armor 아이템 활성화 시 
 		if (armorFlag) {
 			armorTimer--;
+
+			//시간이 적게 남았을수록 깜빡거리는 횟수 증가
 			if (armorTimer == 200 || armorTimer == 100 || armorTimer == 50 || armorTimer == 30 || armorTimer == 10) 
 				cannonChange(false);
 			else if (armorTimer == 150 || armorTimer == 75 || armorTimer == 40 || armorTimer == 20 || armorTimer == 5) 
@@ -500,7 +481,7 @@ void timerCallBack(TimerID timer) {
 			}
 		}
 
-		//power timer
+		//X2 아이템 활성화 시
 		if (powerFlag) {
 			powerTimer--;
 			if (powerTimer <= 0) {
@@ -512,7 +493,6 @@ void timerCallBack(TimerID timer) {
 		locateBall();
 		locateBullet();
 		locateItem();
-		
 		hitBullet();
 		bulletInSky();
 		itemCheck();
@@ -528,7 +508,6 @@ void timerCallBack(TimerID timer) {
 		setTimer(endTimer, 3.0f);
 		stageButton();
 		enterScene(selectScene);
-
 		hideObject(clearImage);
 		hideObject(failImage);
 
@@ -536,12 +515,10 @@ void timerCallBack(TimerID timer) {
 			balls[i].life = false;
 			hideObject(balls[i].object);
 		}
-
 		for (int i = 0; i < BULLETNUMBER; i++) {
 			bullets[i].life = false;
 			hideObject(bullets[i].object);
 		}	
-
 		for (int i = 0; i < 2; i++) {
 			items[i].exist = false;
 			items[i].used = false;
@@ -551,7 +528,7 @@ void timerCallBack(TimerID timer) {
 }
 
 int main() {
-	srand(time(NULL));	//rand()
+	srand(time(NULL));	//rand() 함수
 
 	setMouseCallback(mouseCallBack);
 	setTimerCallback(timerCallBack);
